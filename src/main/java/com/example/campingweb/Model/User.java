6f21c77d8b23abb01ee6028deb7ee6e1f958b9f7
Jihee Hong <486480@student.saxion.nl>
2020-06-11 15:29:04 +0200

    user class added

---
+++ src/main/java/com/example/campingweb/Model/User.java
@@ -0,0 +1,48 @@
+package com.example.campingweb.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private List<CampingPark> favoriteList;
+
+    public User(String firstName, String lastName, String userName, String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        favoriteList = new ArrayList<>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public List<CampingPark> getFavoriteList() {
+        return favoriteList;
+    }
+
+    public void addCampingPark(CampingPark campingPark){
+        favoriteList.add(campingPark);
+    }
+    
+    public void removeCampingPark(CampingPark campingPark){
+        for(CampingPark park : favoriteList){
+            if(park==campingPark){
+                favoriteList.remove(campingPark);
+            }
+        }
+    }
+}
